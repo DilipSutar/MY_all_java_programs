3917a9d6c760cdea54363020270571b3e4bbf36c
DilipSutar <dilipsutar197043@gmail.com>
2023-05-18 22:58:55 +0530

    update

--- src/Array_concepts/PairsInArray.java
+++ src/Array_concepts/PairsInArray.java
@@ -2,13 +2,16 @@ package Array_concepts;
 //we have to us nested loops
 public class PairsInArray {
     public static void PairsOfArray(int numbers[]){
+        int totalpair=0;
         for(int i=0;i<numbers.length;i++){
             int current=numbers[i];
             for(int j=i+1;j<numbers.length;j++){
-                System.out.print("("+current+","+numbers[j]+")");
+                System.out.print("("+current+","+numbers[j]+") ");
+                totalpair++;//total pairs formula is (n(n-1))/2
             }
             System.out.println();
         }
+        System.out.println("total pairs are :"+totalpair);
     }
     public static void main(String[] args) {
         int numbers[]={2,4,6,8,10};
