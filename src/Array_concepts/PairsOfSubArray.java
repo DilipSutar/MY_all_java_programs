12399e093e2ef87411c2141c868699419269b5c6
DilipSutar <dilipsutar197043@gmail.com>
2023-05-19 00:41:11 +0530

    maxSubArraySum

--- src/Array_concepts/PairsOfSubArray.java
+++ src/Array_concepts/PairsOfSubArray.java
@@ -9,14 +9,34 @@ public class PairsOfSubArray {
                 int end=j;
                 for(int k=start;k<=end;k++){
                     System.out.print(numbers [k]+" ");
-                }         
+                    }         
                 System.out.println();
             }
             System.out.println();
         }
     }
+    public static void maxSubArray(int numbers[]){
+        int current=0;
+        int maxsum=Integer.MIN_VALUE;
+        for(int i=0;i<numbers.length;i++){
+             int start=i;
+             for(int j=i;j<numbers.length;j++){
+                int end=j;
+                current=0;
+                for(int k=start;k<=end;k++){
+                    current +=numbers[k];
+                }
+                System.out.println(current);
+                if(maxsum<current){
+                    maxsum=current;
+                }
+             }
+        }
+        System.out.println("max sum : "+maxsum);
+    }
     public static void main(String[] args) {
         int numbers[]={2,4,6,8,10};
         SubArray(numbers);
+        maxSubArray(numbers);
     }
 }
